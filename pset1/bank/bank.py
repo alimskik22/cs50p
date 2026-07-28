5e16749186dc9d14cb9949d352563b364a97873f
Alima Karimova <9yn2fjft6y@privaterelay.appleid.com>
2026-07-28 16:55:13 +0500

    Implement greeting-based bank fee calculation

---
+++ pset1/bank/bank.py
@@ -0,0 +1,9 @@
+greet = input("Greeting: ")
+greet = greet.lower().strip()
+
+if greet.startswith("hello"):
+    print("$0")
+elif greet.startswith("h") and greet != "hello":
+    print("$20")
+else:
+    print("$100")
