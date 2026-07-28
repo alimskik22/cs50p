6bbe550c2bbffa80a306499ad155b2d5bc468a4b
Alima Karimova <9yn2fjft6y@privaterelay.appleid.com>
2026-07-28 17:34:31 +0500

    Add unit tests for count function in test_um.py

---
+++ pset7/um/test_um.py
@@ -0,0 +1,26 @@
+import pytest
+from um import count
+
+
+def test_single():
+    assert count("um") == 1
+    assert count("Um") == 1
+    assert count("UM") == 1
+
+
+
+def test_multiple():
+    assert count("um um um") == 3
+    assert count("Um, thanks, um.") == 2
+
+
+def test_not_substring():
+    assert count("yummy") == 0
+    assert count("umbrella") == 0
+
+
+def test_punctuation():
+    assert count("(um)") == 1
+    assert count("um? um! um.") == 3
+
+
